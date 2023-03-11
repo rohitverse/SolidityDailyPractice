1b26c90f59dc69f5dda1f45e4abae963044764e9
KR ROHIT <67726628+rohitverse@users.noreply.github.com>
2023-03-11 21:14:52 +0530

    Added DataType Program

---
+++ 005-Datatype.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+        
+contract DataTypes  {
+                bool public isTrue=true;
+                uint256 public number=123;
+                address public wallet = 0x25C8a7c90C91C487CdB80f6247f6f9293844c60b;
+                string public line ="Rohit";
+}
